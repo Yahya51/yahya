336423e8cfcc85d5d4268e46ee23e8b0fd07fce1
Yahya Naseem <121633168+Yahya51@users.noreply.github.com>
2026-02-12 15:55:55 +0000

    conversion of datatype

---
+++ problem1/conversion.js
@@ -0,0 +1,21 @@
+let score= true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// 33=> 33
+// 33ab=> Nan
+// true=>1 and false=>0
+// undefined => nan
+// null=>0
+let isLoggedIn= "yahya"//""//1
+
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber=33
+let stringNumber= String(someNumber)
+console.log(typeof stringNumber);
