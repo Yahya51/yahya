98bcba3dd94bfc19b1ab6ee708114b8c55b4e0a3
Yahya Naseem <121633168+Yahya51@users.noreply.github.com>
2026-02-12 07:37:48 +0000

    update

--- problem1/datatype.js
+++ problem1/datatype.js
@@ -1,3 +1,19 @@
 "use strict"; // treat all js code as newer version
 // alert(3+3) we are using node.js not browser
 console.log(3+3)
+let name= "yahya"
+let age= 18
+let isLoggerIn= false
+
+// number => 2 to power 53
+// bigint
+// string=> ""
+// boolean => true/false
+// null
+// undefined =>
+// symbol=> unique
+// object
+
+console.log(typeof "yahya");
+console.log(typeof null);
+console.log(typeof undefined);
