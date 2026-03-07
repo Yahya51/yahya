613927dd9d4f911a02e3ac29576c8dcb044faec0
Yahya Naseem <121633168+Yahya51@users.noreply.github.com>
2026-03-07 07:17:07 +0000

    scopes

---
+++ problem1/scopes.js
@@ -0,0 +1,55 @@
+// var c = 300
+let a= 100
+if (true){
+   let a = 10
+   const b= 20
+   var c = 30 
+//    console.log("Inner", a);
+   
+}
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username= "yahya"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+        
+    }
+    // console.log(website);
+
+    two()
+    
+}
+// one()
+
+if(true){
+    const username ="yahya"
+    if(username === "yahya"){
+        const website ="youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// ++++++++ interesting ++++++
+console.log(addone(5))
+
+function addone(num){
+    return num + 1;
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num +2;
+}
+
